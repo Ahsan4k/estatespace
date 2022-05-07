726010a4ace331c9e09298921502b1a20aa9acb9
Ahsan4k <83755681+Ahsan4k@users.noreply.github.com>
2022-05-07 14:35:20 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,145 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+  Modal,
+  TextInput,
+  Button,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
+import React from 'react';
+import {scale} from 'react-native-size-matters';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+const App = () => {
+  const [data, setData] = React.useState([]);
+  const [loading, isLoading] = React.useState(false);
+  const [count, setCount] = React.useState('');
+  const [page, setPage] = React.useState('');
+  const [modalVisible, isModalVisible] = React.useState(false);
+
+  const someFunc = async () => {
+    if (page === '' || count === '') {
+      Alert.alert('Sorry', 'Please enter a number', [{text: 'OK'}]);
+    } else {
+      try {
+        isLoading(true);
+        const response = await fetch(
+          `https://my.api.mockaroo.com/users.json?page=${count}&count=${page}&key=930279b0`,
+          {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+        const result = await response.json();
+        console.log('result', result);
+        if (response) {
+          setData(result.entries);
+          isModalVisible(false);
+        }
+      } catch (err) {
+        Alert.alert(`${err.name}`, `${err.message}`, [{text: 'OK'}]);
+      }
+      isLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.load}>
+        <ActivityIndicator color={'blue'} size={'large'} />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.background}>
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View style={styles.modal}>
+          <View style={styles.inner}>
+            <Text>Input Count</Text>
+            <TextInput
+              style={styles.ti}
+              onChangeText={text => setCount(text)}
+              value={count}
+              keyboardType="number-pad"
+            />
+            <Text>Input Page</Text>
+            <TextInput
+              style={styles.ti}
+              onChangeText={text => setPage(text)}
+              value={page}
+              keyboardType="number-pad"
+            />
+            <Button title="Submit" onPress={someFunc} />
+          </View>
+        </View>
+      </Modal>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Button title="FInd Entries" onPress={() => isModalVisible(true)} />
+        {data.map((item, index) => (
+          <View style={styles.iterate} key={index}>
+            <Text>
+              {item.name.firstName} {item.name.lastName}
+            </Text>
+            <Text>{item.email}</Text>
+            <Text>{item.gender}</Text>
+            <Text>{item.role}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  background: {
+    width: width,
+    height: height,
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  modal: {
+    width: width,
+    height: height,
+    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ti: {
+    backgroundColor: 'white',
+    width: width * 0.8,
+    height: height * 0.02,
+    borderWidth: scale(1),
+  },
+  inner: {
+    backgroundColor: 'lightgrey',
+    width: width * 0.9,
+    height: height * 0.2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  load: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iterate: {
+    width: width * 0.9,
+    height: height * 0.2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: height * 0.02,
+    backgroundColor: 'lightgrey',
+  },
+});
